d16eb5e7a8764eade77e32ba59d69a5e1e06a8ac
CarlosR <carlosromero.biz@gmail.com>
2021-11-09 21:25:32 -0700

    css updates, add logo

--- Twitter_Clone/urls.py
+++ Twitter_Clone/urls.py
@@ -1,4 +1,4 @@
-"""django_forum URL Configuration
+"""Twitter_Clone URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
